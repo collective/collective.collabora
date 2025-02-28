716e2486cc76b24cef7864d279415491ec0d55e9
Guido A.J. Stevens <guido.stevens@cosent.nl>
2025-02-28 22:21:56 +0100

    Separate WOPI callback from editor GUI

--- src/collective/collabora/browser/cool_editor.py
+++ src/collective/collabora/browser/cool_editor.py
@@ -1,19 +1,12 @@
 from collective.collabora import _
-from datetime import datetime
 from logging import getLogger
 from lxml import etree
 from plone import api
 from plone.app.contenttypes.browser.file import FileView
 from plone.memoize.view import memoize
-from plone.namedfile.file import NamedBlobFile
-from plone.protect.interfaces import IDisableCSRFProtection
 from plone.uuid.interfaces import IUUID
 from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
-from zope.interface import alsoProvides
-from zope.interface import implementer
-from zope.publisher.interfaces import IPublishTraverse
 
-import json
 import requests
 import urllib.parse
 
@@ -21,63 +14,16 @@ import urllib.parse
 logger = getLogger(__name__)
 
 
-@implementer(IPublishTraverse)
 class CoolEditorView(FileView):
-    """LibreOffice / Collabora editor view."""
+    """User interface for interacting with Collabora Online."""
 
     def __init__(self, context, request):
         super().__init__(context, request)
-        self.wopi_mode = None
         self.error_msg = None
 
-    def publishTraverse(self, request, name, *args, **kwargs):
-        """Provide the WOPI endpoints:
-
-            - @@cool_editor/wopi/files/<id>
-            - @@cool_editor/wopi/files/<id>/contents
-
-        or fall back to the default view.
-        """
-        logger.debug(
-            "publishTraverse(): %s, %s, %s (url=%s)",
-            name,
-            args,
-            kwargs,
-            request.environ["REQUEST_URI"],
-        )
-        parts = request.environ["REQUEST_URI"].split("/")
-        if parts[-3] == "wopi" and parts[-2] == "files" and name == IUUID(self.context):
-            self.wopi_mode = "file_info"
-        elif (
-            parts[-4] == "wopi"
-            and parts[-3] == "files"
-            and parts[-2] == IUUID(self.context)
-            and name == "contents"
-        ):
-            self.wopi_mode = "contents"
-        if self.wopi_mode:
-            logger.debug("WOPI mode: %s %s", self.request.method, self.wopi_mode)
-        return self
-
     def __call__(self):
-        logger.debug(
-            "EditorView(): %s %s",
-            self.request.method,
-            self.request.environ["REQUEST_URI"],
-        )
-        if not self.server_url or not self.portal_url:
-            # just checking it activates the error_msg
-            pass
-        elif self.wopi_mode == "file_info":
-            return self.wopi_check_file_info()
-        elif self.wopi_mode == "contents":
-            if self.request.method == "GET":
-                return self.wopi_get_file()
-            elif self.request.method == "POST":
-                return self.wopi_put_file()
-        elif not self.wopi_url:
-            # just calling this ensures error_msg is initialized
-            # if needed
+        if not all([self.portal_url, self.server_url, self.wopi_url]):
+            # accessing those detects errors and sets self.error_msg
             pass
         return super().__call__()
 
@@ -88,119 +34,20 @@ class CoolEditorView(FileView):
             "Modify portal content", user=api.user.get_current(), obj=self.context
         )
 
-    def wopi_get_file(self):
-        """WOPI GetFile endpoint.
-
-        Return the file content.
-        """
-        logger.debug("wopi_get_file: %s", self.context.absolute_url())
-        # TODO: CORS header actually not needed.
-        self.request.response.setHeader("Access-Control-Allow-Origin", "*")
-
-        return self.context.file.data
-
-    def wopi_check_file_info(self):
-        """WOPI CheckFileInfo endpoint.
-
-        Return the file information.
-        """
-        logger.debug("wopi_check_file_info: %s", self.context.absolute_url())
-        # TODO: CORS header actually not needed.
-        self.request.response.setHeader("Access-Control-Allow-Origin", "*")
-        self.request.response.setHeader("Content-Type", "application/json")
-
-        file = self.context.file
-        user = api.user.get_current()
-        user_id = user.getId()
-        file_info = {
-            "BaseFileName": file.filename,
-            "Size": file.getSize(),
-            "OwnerId": self.context.getOwner().getId(),
-            "UserId": user_id,
-            "UserCanWrite": self.can_edit,
-            "UserFriendlyName": user.getProperty("fullname") or user_id,
-            "UserCanNotWriteRelative": True,  # No "Save As" button
-            "LastModifiedTime": self.context.modified().ISO8601(),
-            "PostMessageOrigin": self.context.absolute_url(),
-        }
-        logger.debug(
-            "file_info: %s %s %s: %s",
-            user_id,
-            self.can_edit and "can edit" or "can not edit",
-            self.context.absolute_url(),
-            file_info,
-        )
-        return json.dumps(file_info)
-
-    def wopi_put_file(self):
-        """WOPI PutFile endpoint.
-
-        Update the file content.
-        """
-        logger.debug("wopi_put_file: %s", self.context.absolute_url())
-        self.request.response.setHeader("Content-Type", "application/json")
-
-        # TODO:
-        # - Check locking (see ploneintranet.workspace.basecontent.baseviews.BaseView)
-        # - Check autosave (see same)
-        # - Should we derive from BaseView and also use reset_dx_modified?
-        # - Use ploneintranet.workspace.basecontent.utils.dexterity_update?
-
-        # https://sdk.collaboraonline.com/docs/advanced_integration.html#putfile-headers
-        # Relevant headers:
-        # - HTTP_X_COOL_WOPI_ISAUTOSAVE
-        # - HTTP_X_COOL_WOPI_ISEXITSAVE
-        # - HTTP_X_COOL_WOPI_ISMODIFIEDBYUSER
-        # - HTTP_X_COOL_WOPI_TIMESTAMP
-        user_timestamp = self.request.get("HTTP_X_COOL_WOPI_TIMESTAMP", None)
-        if user_timestamp:
-            # Document modified by another user. Return and let LibreOffice /
-            # Collabora ask the user to overwrite or not. If called again
-            # without a HTTP_X_COOL_WOPI_TIMESTAMP, the document is saved
-            # regardless of the modification status.
-            #
-            # See:
-            # https://sdk.collaboraonline.com/docs/advanced_integration.html#detecting-external-document-change  # noqa: E501
-            #
-            user_dt = datetime.fromisoformat(user_timestamp)
-            context_timestamp = self.context.modified().ISO8601()
-            context_dt = datetime.fromisoformat(context_timestamp)
-
-            if context_dt > user_dt:
-                logger.debug(
-                    "User changes are outdated. User: <%s>. URL: <%s>.",
-                    api.user.get_current().getId(),
-                    self.context.absolute_url(),
-                )
-
-                self.request.response.setStatus(409)
-                return json.dumps({"COOLStatusCode": 1010})
-
-        if self.request.get("HTTP_X_COOL_WOPI_ISMODIFIEDBYUSER", False):
-            # Save changes back, if document was modified.
-
-            # TODO, maybe do not disable CSRF protection here?
-            alsoProvides(self.request, IDisableCSRFProtection)
-
-            file = self.context.file
-            filename = file.filename
-            content_type = file.contentType
-            data = self.request._file.read()
-            self.context.file = NamedBlobFile(
-                data=data, filename=filename, contentType=content_type
-            )
-
-            logger.debug(
-                "File updated. User: <%s>. URL: <%s>.",
-                api.user.get_current().getId(),
+    @property
+    @memoize
+    def download_url(self):
+        return "/".join(
+            (
                 self.context.absolute_url(),
+                "@@download",
+                "file",
+                self.context.file.filename,
             )
-
-        # Success.
-        self.request.response.setStatus(200)
-        return json.dumps({})
+        )
 
     @property
+    @memoize
     def portal_url(self):
         portal_url = api.portal.get().absolute_url()
         parts = urllib.parse.urlparse(portal_url)
@@ -217,17 +64,7 @@ class CoolEditorView(FileView):
         return portal_url
 
     @property
-    def download_url(self):
-        return "/".join(
-            (
-                self.context.absolute_url(),
-                "@@download",
-                "file",
-                self.context.file.filename,
-            )
-        )
-
-    @property
+    @memoize
     def server_url(self):
         server_url = api.portal.get_registry_record(
             "collective.collabora.server_url", default=None
@@ -241,21 +78,7 @@ class CoolEditorView(FileView):
         return server_url
 
     @property
-    def wopi_url(self):
-        """Return the URL to load the document in LibreOffice / Collabora."""
-        editor_url = self.editor_url
-        if not editor_url:
-            return None
-        document_url = self.context.absolute_url()
-        uuid = IUUID(self.context)
-        args = dict(
-            WOPISrc=f"{document_url}/@@cool_editor/wopi/files/{uuid}",
-            access_token=self.jwt_token,
-        )
-        quoted_args = urllib.parse.urlencode(args)
-        return f"{editor_url}{quoted_args}"
-
-    @property
+    @memoize
     def editor_url(self):
         """Return the URL of the LibreOffice / Collabora editor.
 
@@ -296,6 +119,7 @@ class CoolEditorView(FileView):
         return urlsrc
 
     @property
+    @memoize
     def jwt_token(self):
         """Return a JWT token from the plone.restapi JWT PAS plugin.
 
@@ -317,3 +141,18 @@ class CoolEditorView(FileView):
             logger.error("JWT Authentication Plugin not found.")
             return None
         return jwt_plugin.create_token(api.user.get_current().getId())
+
+    @property
+    @memoize
+    def wopi_url(self):
+        """Return the URL to load the document in LibreOffice / Collabora."""
+        if not self.editor_url or not self.jwt_token:
+            return None
+        document_url = self.context.absolute_url()
+        uuid = IUUID(self.context)
+        args = dict(
+            WOPISrc=f"{document_url}/@@cool_wopi/files/{uuid}",
+            access_token=self.jwt_token,
+        )
+        quoted_args = urllib.parse.urlencode(args)
+        return f"{self.editor_url}{quoted_args}"
---
+++ src/collective/collabora/browser/cool_wopi.py
@@ -0,0 +1,185 @@
+from datetime import datetime
+from logging import getLogger
+from plone import api
+from plone.memoize.view import memoize
+from plone.namedfile.file import NamedBlobFile
+from plone.protect.interfaces import IDisableCSRFProtection
+from plone.uuid.interfaces import IUUID
+from Products.Five.browser import BrowserView
+from urllib.parse import urlparse
+from zope.interface import alsoProvides
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+
+import json
+
+
+logger = getLogger(__name__)
+
+
+@implementer(IPublishTraverse)
+class CoolWOPIView(BrowserView):
+    """Callback view used by Collabora Online to talk to Plone"""
+
+    def __init__(self, context, request):
+        super().__init__(context, request)
+        self.wopi_mode = None
+
+    def publishTraverse(self, request, name, *args, **kwargs):
+        """Provide the WOPI endpoints:
+
+        - @@cool_wopi/files/<uid>
+        - @@cool_wopi/files/<uid>/contents
+        """
+        parts = urlparse(request.environ["REQUEST_URI"]).path.split("/")
+        # This is traversed for each part of the URL. Make sure to catch only
+        # the "last" traversal, by checking the position of "@@cool_wopi".
+        if parts[-3] == "@@cool_wopi" and name == IUUID(self.context):
+            self.wopi_mode = "file_info"
+        elif parts[-4] == "@@cool_wopi" and name == "contents":
+            self.wopi_mode = "contents"
+            assert parts[-2] == IUUID(self.context)
+        logger.debug(
+            "publishTraverse(): %s, %s, %s (url=%s): wopi_mode = %s",
+            name,
+            args,
+            kwargs,
+            request.environ["REQUEST_URI"],
+            self.wopi_mode,
+        )
+        return self
+
+    def __call__(self):
+        logger.debug(
+            "%s: %s %s: wopi_mode = %s",
+            self.__class__.__name__,
+            self.request.method,
+            self.request.environ["REQUEST_URI"],
+            self.wopi_mode,
+        )
+        if self.wopi_mode == "file_info":
+            return self.wopi_check_file_info()
+        assert self.wopi_mode == "contents"
+        if self.request.method == "GET":
+            return self.wopi_get_file()
+        if self.request.method == "POST":
+            return self.wopi_put_file()
+
+    @property
+    @memoize
+    def can_edit(self):
+        return api.user.has_permission(
+            "Modify portal content", user=api.user.get_current(), obj=self.context
+        )
+
+    def wopi_check_file_info(self):
+        """WOPI CheckFileInfo endpoint.
+
+        Return the file information.
+        """
+        logger.debug("wopi_check_file_info: %s", self.context.absolute_url())
+        # TODO: CORS header actually not needed.
+        self.request.response.setHeader("Access-Control-Allow-Origin", "*")
+        self.request.response.setHeader("Content-Type", "application/json")
+
+        file = self.context.file
+        user = api.user.get_current()
+        user_id = user.getId()
+        file_info = {
+            "BaseFileName": file.filename,
+            "Size": file.getSize(),
+            "OwnerId": self.context.getOwner().getId(),
+            "UserId": user_id,
+            "UserCanWrite": self.can_edit,
+            "UserFriendlyName": user.getProperty("fullname") or user_id,
+            "UserCanNotWriteRelative": True,  # No "Save As" button
+            "LastModifiedTime": self.context.modified().ISO8601(),
+            "PostMessageOrigin": self.context.absolute_url(),
+        }
+        logger.debug(
+            "file_info: %s %s %s: %s",
+            user_id,
+            self.can_edit and "can edit" or "can not edit",
+            self.context.absolute_url(),
+            file_info,
+        )
+        return json.dumps(file_info)
+
+    def wopi_get_file(self):
+        """WOPI GetFile endpoint.
+
+        Return the file content.
+        """
+        logger.debug("wopi_get_file: %s", self.context.absolute_url())
+        # TODO: CORS header actually not needed.
+        self.request.response.setHeader("Access-Control-Allow-Origin", "*")
+
+        return self.context.file.data
+
+    def wopi_put_file(self):
+        """WOPI PutFile endpoint.
+
+        Update the file content.
+        """
+        logger.debug("wopi_put_file: %s", self.context.absolute_url())
+        self.request.response.setHeader("Content-Type", "application/json")
+
+        # TODO:
+        # - Check locking (see ploneintranet.workspace.basecontent.baseviews.BaseView)
+        # - Check autosave (see same)
+        # - Should we derive from BaseView and also use reset_dx_modified?
+        # - Use ploneintranet.workspace.basecontent.utils.dexterity_update?
+
+        # https://sdk.collaboraonline.com/docs/advanced_integration.html#putfile-headers
+        # Relevant headers:
+        # - HTTP_X_COOL_WOPI_ISAUTOSAVE
+        # - HTTP_X_COOL_WOPI_ISEXITSAVE
+        # - HTTP_X_COOL_WOPI_ISMODIFIEDBYUSER
+        # - HTTP_X_COOL_WOPI_TIMESTAMP
+        user_timestamp = self.request.get("HTTP_X_COOL_WOPI_TIMESTAMP", None)
+        if user_timestamp:
+            # Document modified by another user. Return and let LibreOffice /
+            # Collabora ask the user to overwrite or not. If called again
+            # without a HTTP_X_COOL_WOPI_TIMESTAMP, the document is saved
+            # regardless of the modification status.
+            #
+            # See:
+            # https://sdk.collaboraonline.com/docs/advanced_integration.html#detecting-external-document-change  # noqa: E501
+            #
+            user_dt = datetime.fromisoformat(user_timestamp)
+            context_timestamp = self.context.modified().ISO8601()
+            context_dt = datetime.fromisoformat(context_timestamp)
+
+            if context_dt > user_dt:
+                logger.debug(
+                    "User changes are outdated. User: <%s>. URL: <%s>.",
+                    api.user.get_current().getId(),
+                    self.context.absolute_url(),
+                )
+
+                self.request.response.setStatus(409)
+                return json.dumps({"COOLStatusCode": 1010})
+
+        if self.request.get("HTTP_X_COOL_WOPI_ISMODIFIEDBYUSER", False):
+            # Save changes back, if document was modified.
+
+            # TODO, maybe do not disable CSRF protection here?
+            alsoProvides(self.request, IDisableCSRFProtection)
+
+            file = self.context.file
+            filename = file.filename
+            content_type = file.contentType
+            data = self.request._file.read()
+            self.context.file = NamedBlobFile(
+                data=data, filename=filename, contentType=content_type
+            )
+
+            logger.debug(
+                "File updated. User: <%s>. URL: <%s>.",
+                api.user.get_current().getId(),
+                self.context.absolute_url(),
+            )
+
+        # Success.
+        self.request.response.setStatus(200)
+        return json.dumps({})
